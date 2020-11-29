1d533c47cce8df836d7271d5dcf365585d3b7480
Patrick Behrens <75225423+DerPartyPatrick@users.noreply.github.com>
2020-11-30 00:19:20 +0100

    Add files via upload

---
+++ Scripts/MaceController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaceController : MonoBehaviour
+{
+    public bool isGrounded;
+    public bool startPosition; 
+    public LayerMask groundLayers;
+    public int stompDelay;
+    public int stompSpeed;
+    public int groundTime;
+    public int upSpeed;
+    public float offset; 
+
+    private float width;
+    private float height;
+    private Rigidbody2D rb;
+    private float startY;
+    private bool isStomping;
+    private bool stopped;
+    private bool wait; 
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); 
+        isGrounded = false;
+        isStomping = false;
+        startPosition = true;
+        stopped = false;
+        wait = true; 
+        width = GetComponent<Renderer>().bounds.size.x;
+        height = GetComponent<Renderer>().bounds.size.y;
+        startY = transform.position.y;
+
+        StartCoroutine(Wait()); 
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapArea(
+            new Vector2(transform.position.x - width / 2, transform.position.y - height / 2),
+            new Vector2(transform.position.x + width / 2, transform.position.y - (height / 2 + 0.001f)),
+            groundLayers);
+
+        startPosition = transform.position.y >= startY;
+
+        if((isGrounded || startPosition) && !stopped)
+        {
+            rb.velocity = new Vector2(0, 0);
+            stopped = true; 
+        }
+
+        if(!isGrounded && !startPosition)
+        {
+            stopped = false; 
+        }
+
+        if (!isStomping && !wait)
+        {
+            isStomping = true; 
+            StartCoroutine(Stomp());
+        }
+    }
+
+    IEnumerator Stomp()
+    {
+        yield return new WaitForSeconds(stompDelay); 
+        rb.velocity = new Vector2(0, -stompSpeed);
+        yield return new WaitUntil(() => isGrounded);
+        yield return new WaitForSeconds(groundTime);
+        rb.velocity = new Vector2(0, upSpeed);
+        yield return new WaitUntil(() => startPosition);
+        isStomping = false; 
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(offset);
+        wait = false; 
+    }
+}
---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class PlayerController : MonoBehaviour
+{
+    public int speed;
+    public int jumpForce;
+    public int resetPoint; 
+
+    public bool isGrounded;
+    public LayerMask groundLayers;
+    public Text text;
+    public GameObject coinParent;
+
+    private int goal;
+    private int coins; 
+    private Animator animator;
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+
+    private void Start()
+    {
+        goal = coinParent.transform.childCount; 
+        isGrounded = false;
+        coins = 0;  
+        text.text = "Coins: 0/" + goal; 
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>(); 
+    }
+
+    private void Update()
+    {
+        if(transform.position.y < resetPoint)
+        {
+            Restart(); 
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapArea(
+            new Vector2(transform.position.x - 0.5f, transform.position.y - 0.5f),
+            new Vector2(transform.position.x + 0.5f, transform.position.y - 1f),
+            groundLayers); 
+
+        if(Input.GetKey("a"))
+        {
+            if(isGrounded)
+            {
+                rb.velocity = new Vector2(-speed, 0);
+                animator.Play("PlayerRun");
+            }
+            else
+            {
+                rb.velocity = new Vector2(-speed, rb.velocity.y);
+            }
+
+            sr.flipX = true; 
+        } 
+        else if(Input.GetKey("d"))
+        {
+            if(isGrounded)
+            {
+                rb.velocity = new Vector2(speed, 0);
+                animator.Play("PlayerRun");
+            }
+            else
+            {
+                rb.velocity = new Vector2(speed, rb.velocity.y);
+            }
+
+            sr.flipX = false; 
+        }
+        else
+        {
+            if(isGrounded)
+            {
+                rb.velocity = new Vector2(0, 0);
+                animator.Play("PlayerAnimation");
+            }
+            else
+            {
+                rb.velocity = new Vector2(0, rb.velocity.y);
+            }
+        }
+
+        if(Input.GetKey("space") && isGrounded)
+        {
+            rb.AddForce(new Vector2(0, jumpForce)); 
+        }
+
+        if(!isGrounded)
+        {
+            if(rb.velocity.y > 0)
+            {
+                animator.Play("PlayerJump"); 
+            }
+            else
+            {
+                animator.Play("PlayerFall"); 
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Coin"))
+        {
+            collision.gameObject.SetActive(false);
+            coins++;
+            text.text = "Coins: " + coins + "/" + goal; 
+        }
+
+        if(collision.CompareTag("Saw") || collision.CompareTag("Mace") || collision.CompareTag("Spikes"))
+        {
+            Restart(); 
+        }
+    }
+
+    private void Restart()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
